17d8436933a3bfcb0e7be334324b6fc528693b2b
panzhenglong <120267583@qq.com>
2020-07-28 20:12:54 +0800

    fix

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ module.exports = function (source, map) {
   let ns = source
   if (source.indexOf('webpackChunkName') != -1) {
     ns = `${source.replace(/\/\* webpackChunkName: ".*" \*\//g, `/* webpackChunkName: "page" */`)}`
+    console.log(ns)
   }
   this.callback(
     null,
-    source,
+    ns,
     map
   )
 }
+
